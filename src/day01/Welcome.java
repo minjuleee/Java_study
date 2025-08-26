006209a4a97f55763c04538111d0b825eed392a0
minjuleee <minju2138@hansung.ac.kr>
2025-08-26 21:03:35 +0900

    Java day01 공부

---
+++ src/day01/Welcome.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to my house!");
+		System.out.println("Welcome True?");
+		System.out.println("Not True.");
+	}
+
+}
